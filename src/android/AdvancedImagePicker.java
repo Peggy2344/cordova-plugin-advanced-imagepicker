fd61da0127378d657aa478c76d6a63f75acb4c53
Peggy2344 <peggy102104@gmail.com>
2021-07-24 17:02:57 +0800

    feat:add compress image feature

--- src/android/AdvancedImagePicker.java
+++ src/android/AdvancedImagePicker.java
@@ -1,9 +1,12 @@
 package de.einfachhans.AdvancedImagePicker;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.util.Base64;
+import android.os.AsyncTask;
+import android.app.ProgressDialog;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -15,12 +18,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import gun0912.tedimagepicker.builder.TedImagePicker;
+import com.bumptech.glide.Glide;
+import top.zibin.luban.Luban;
 
 public class AdvancedImagePicker extends CordovaPlugin {
 
@@ -29,7 +35,6 @@ public class AdvancedImagePicker extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this._callbackContext = callbackContext;
-
         try {
             if (action.equals("present")) {
                 this.presentFullScreen(args);
@@ -59,9 +64,10 @@ public class AdvancedImagePicker extends CordovaPlugin {
         String defaultMinCountMessage = "You need to select a minimum of " + (min == 1 ? "one picture" : min + " pictures");
         String minCountMessage = options.optString("minCountMessage", defaultMinCountMessage);
         int max = options.optInt("max");
-        String defaultMaxCountMessage = "You can select a maximum of " + max + " pictures";
+        String defaultMaxCountMessage = "最多只能選擇" + max + "張圖片";
         String maxCountMessage = options.optString("maxCountMessage", defaultMaxCountMessage);
         String buttonText = options.optString("buttonText");
+        String buttonBackground = options.optString("buttonBackground");
         boolean asDropdown = options.optBoolean("asDropdown");
         boolean asBase64 = options.optBoolean("asBase64");
         boolean asJpeg = options.optBoolean("asJpeg");
@@ -92,6 +98,10 @@ public class AdvancedImagePicker extends CordovaPlugin {
         }
         if (!buttonText.equals("")) {
             builder.buttonText(buttonText);
+        }
+        if (!buttonBackground.equals("")) {
+            int defaultImage = this.cordova.getActivity().getResources().getIdentifier("bg_btn", "drawable", this.cordova.getActivity().getPackageName());
+            builder.buttonBackground(defaultImage);
         }
         if (asDropdown) {
             builder.dropDownAlbum();
@@ -125,30 +135,16 @@ public class AdvancedImagePicker extends CordovaPlugin {
     private void handleResult(Uri uri, boolean asBase64, String type, boolean asJpeg) {
         List<Uri> list = new ArrayList<>();
         list.add(uri);
-        this.handleResult(list, asBase64, type, asJpeg);
     }
 
     private void handleResult(List<? extends Uri> uris, boolean asBase64, String type, boolean asJpeg) {
-        JSONArray result = new JSONArray();
+        List<Uri> list = new ArrayList<>();
         for (Uri uri : uris) {
-            Map<String, Object> resultMap = new HashMap<>();
-            resultMap.put("type", type);
-            resultMap.put("isBase64", asBase64);
-            if (asBase64) {
-                try {
-                    resultMap.put("src", type.equals("video") ? this.encodeVideo(uri) : this.encodeImage(uri, asJpeg));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    this.returnError(AdvancedImagePickerErrorCodes.UnknownError, e.getMessage());
-                    return;
-                }
-            } else {
-                resultMap.put("src", uri.toString());
-            }
-            result.put(new JSONObject(resultMap));
+            list.add(uri);
         }
-        this._callbackContext.success(result);
+        new UploadImagesTask().execute(list);
     }
+
 
     private String encodeVideo(Uri uri) throws IOException {
         final InputStream videoStream = this.cordova.getContext().getContentResolver().openInputStream(uri);
@@ -168,7 +164,7 @@ public class AdvancedImagePicker extends CordovaPlugin {
         final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
         return encodeImage(selectedImage, asJpeg);
     }
-
+    
     private String encodeImage(Bitmap bm, boolean asJpeg) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         if (asJpeg) {
@@ -181,6 +177,7 @@ public class AdvancedImagePicker extends CordovaPlugin {
     }
 
     private void returnError(AdvancedImagePickerErrorCodes errorCode) {
+        Glide.get(cordova.getContext()).clearDiskCache();
         returnError(errorCode, null);
     }
 
@@ -191,6 +188,62 @@ public class AdvancedImagePicker extends CordovaPlugin {
             resultMap.put("message", message == null ? "" : message);
             _callbackContext.error(new JSONObject(resultMap));
             _callbackContext = null;
+            Glide.get(cordova.getContext()).clearDiskCache();
+        }
+    }
+
+    private class UploadImagesTask extends AsyncTask<List<Uri>, Void , JSONArray>{
+            private ProgressDialog progressBar;
+            //進度條元件
+            @Override
+            protected void onPreExecute() {
+                //執行前 設定可以在這邊設定
+                super.onPreExecute();
+
+                progressBar = new ProgressDialog(cordova.getActivity());
+                progressBar.setMessage("圖片加載中，請稍候...");
+                progressBar.setCancelable(false);
+                progressBar.show();
+                //初始化進度條並設定樣式及顯示的資訊。
+            }
+
+            @Override
+            protected JSONArray doInBackground(List<Uri>... files) {
+                JSONArray result = new JSONArray();
+                Context appContext = cordova.getActivity().getApplicationContext();
+                File targetDir = new File(appContext.getCacheDir(), "ImagePicker");
+                if(!targetDir.exists()) {
+                    targetDir.mkdirs();
+                }
+                String targetDirPath = targetDir.getAbsolutePath();
+                List<Uri> uris = files[0];
+                for (Uri uri : uris) {
+                    File oldFile = new File(uri.getPath());
+                    File newFile = null;
+                    Map<String, Object> resultMap = new HashMap<>();
+                    try {
+                        List<File> file = Luban.with(appContext)
+                                                .load(oldFile)
+                                                .setTargetDir(targetDirPath)
+                                                .get();
+                        if(file.size() > 0) {
+                            newFile = file.get(0);
+                        } else {
+                            throw new Exception("Unknown exception when compressing " + oldFile.getAbsolutePath());
+                        }
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+                    resultMap.put("src", newFile.getAbsolutePath());
+                    result.put(new JSONObject(resultMap));
+                }
+                Glide.get(cordova.getContext()).clearDiskCache();
+                return result;
+        }
+
+        protected void onPostExecute(JSONArray result) {
+            progressBar.dismiss();
+            _callbackContext.success(result);
         }
     }
 }
